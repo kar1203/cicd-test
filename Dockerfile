07cea9eabab797a3c2e4be43b61c9c853d390b7a
karkar <yiam.kar.kar@huawei.com>
2023-03-22 22:30:07 +0800

    push new files

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+# docker build . -t my-php-app:1.0.0
+
+FROM php:7.2-fpm
+WORKDIR /var/www/html/
+RUN mkdir /app
+COPY /project/. /app
---
+++ project/index.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <title>Automated CI/CD Demo</title>
+    </head>
+    <style>
+      div {
+        display: table-cell;
+        width: 1400px;
+        height: 800px;
+        font-family:'Courier New';
+        vertical-align: middle;
+        font-size: 2em;
+        text-align: center;
+        color: white;
+      }
+    </style>
+        <!-- <body style="background-color:Red;"> -->
+        <!-- <body style="background-color:DodgerBlue;"> -->
+        <!-- <body style="background-color:#AAFF00;"> -->
+        <!-- <body style="background-color:#088F8F;"> -->
+        <body style="background-color:Pink;">
+        <!-- <body style="background-color:Green;"> -->
+        <!-- <body style="background-color:#FFC0CB;"> -->
+        <!-- <body style="background-color:#A020F0;"> -->
+
+         <div>
+         <h1> www.demo-test.live</h1>
+         <h1>DevOps CI/CD Demo</h1>
+         <!-- <h1> Red v1.13-<?php date_default_timezone_set("Asia/Kuala_Lumpur");echo date("YmdHis");?> </h1> -->
+         <!-- <h1> Blue v1.14-<?php date_default_timezone_set("Asia/Kuala_Lumpur");echo date("YmdHis");?> </h1> -->
+         <!-- <h1> SIT - Green v1.14-<?php date_default_timezone_set("Asia/Kuala_Lumpur");echo date("YmdHis");?> </h1> -->
+         <!-- <h1> PROD - Red v1.13 </h1> -->
+         <!-- <h1> PROD - Blue v1.14 </h1> -->
+         <h1> (Production under Observed) </h1>
+         <h1>Pink Version v3.0</h1>
+        </div>
+    </body>
+</html>
